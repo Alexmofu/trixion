2f2c98109321826e32d3f475e8ff5c939cb9a0f7
Alexander Moreno Fuentes <Alexmofu98@gmail.com>
2022-06-02 19:30:35 +0200

    Added Apache Error Messages

---
+++ errors/404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=0">
+    <title>Trixion - Error 404</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body class="errorbody">
+    <div class="erroranim noselect"><object class="noselect objecterror" data="../errors/trixionerror404.svg" width="350px" height="70%"></object>
+    <div class="noselect backhome"><a href="https://www.trixion.net">Back to Trixion.net</a></div></div>
+    
+</body>
+</html>
